c97b2d6ac153a3e6fe11b6fba5fad5ec9ef25a98
dharasoni16 <99190748+dharasoni16@users.noreply.github.com>
2023-08-07 16:25:35 +0200

    Add: HTML, CSS and JS for To-do-list app

---
+++ js/scripts.js
@@ -0,0 +1,36 @@
+function newItem() {
+  //JQuery
+  //1. Adding a new item to the list of items:
+  let li = $("<li></li>");
+  let inputValue = $("#input").val();
+  li.append(inputValue);
+
+  if (inputValue === "") {
+    alert("You must write something!");
+  } else {
+    $("#list").append(li);
+    $("#input").val("");
+  }
+
+  //2. Crossing out item from the list of items:
+  function crossOut() {
+    li.toggleClass("strike");
+  }
+
+  li.on("dblclick", crossOut);
+
+  //3(i). Adding the delete button 'X':
+  let crossOutButton = $("<crossOutButton></crossOutButton>");
+  crossOutButton.append(document.createTextNode("X"));
+  li.append(crossOutButton);
+
+  crossOutButton.on("click", deleteListItem);
+
+  //3(ii). Adding class delete (display:none) from css:
+  function deleteListItem() {
+    li.addClass("delete");
+  }
+
+  //4. Reordering the items:
+  $("#list").sortable();
+}
